7ca618c72cd2be6466e993e05d66c18508111cd4
Rastislav Svoboda <rastislav.svoboda@gmail.com>
2021-09-10 15:33:12 +0200

    pouzita componenta cez MF (a upraveny styl)

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,17 +1,21 @@
 <template>
   <img alt="Vue logo" src="/assets/logo.png" />
-  <div>
-    Pozor tunel
+  <h2>
+    Hello Vue 3 with Webpack 5 and Module Federation plugin
+  </h2>
+  <div class="container">
+  <HelloWorld />
   </div>
 </template>
 
 <script>
+import HelloWorld from 'module1/HelloWorld'
 
 
 export default {
   name: "App",
   components: {
-    // HelloWorld,
+    HelloWorld,
   },
 };
 </script>
@@ -24,5 +28,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.container {
+  border: 1px dashed red;
+  background: yellow;
 }
 </style>
