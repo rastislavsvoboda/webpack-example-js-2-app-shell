8aa23addf56861a8ebef3933890e64903be0d707
Rastislav Svoboda <rastislav.svoboda@gmail.com>
2021-09-10 16:04:34 +0200

    pridane loga

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,16 +1,20 @@
 <template>
-  <img alt="Vue logo" src="/assets/logo.png" />
+  <div class="logos">
+    <img alt="Vue logo" src="/assets/vue.png" />
+    <img alt="Webpack logo" src="/assets/webpack.png" />
+    <img alt="ModuleFederation logo" src="/assets/mf.jfif" />
+    <img alt="JavaScript logo" src="/assets/js.png" />
+  </div>
   <h2>
-    Hello Vue 3 with Webpack 5 and Module Federation plugin
+    Hello Vue 3 with Webpack 5 and Module Federation plugin and JavaScript
   </h2>
   <div class="container">
-  <HelloWorld />
+    <HelloWorld />
   </div>
 </template>
 
 <script>
-import HelloWorld from 'module1/HelloWorld'
-
+import HelloWorld from "module1/HelloWorld";
 
 export default {
   name: "App",
@@ -30,8 +34,18 @@ export default {
   margin-top: 60px;
 }
 
+.logos {
+  display: flex;
+  align-items: center;
+  justify-items: center;
+  justify-content: space-around;
+  margin: auto;
+}
+
 .container {
   border: 1px dashed red;
   background: yellow;
+  border-radius: 50px;
+  margin: 5px 30px;
 }
 </style>
